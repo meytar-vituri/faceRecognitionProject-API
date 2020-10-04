7753ffc0b49cb0da72e64a183fefb99bff59b28b
meytar <meytarvituri@mail.tau.ac.il>
2020-10-04 12:51:40 +0300

    fix

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const db = knex({
     client: 'pg',
     connection: {
       connectString : process.env.DATABASE_URL,
-      ssl: true  
+      ssl: {
+          rejectUnauthorized: false
+      }  
     }
   });
 
